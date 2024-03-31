eeb46f56037514d0083080e5b76945d9b98780d6
ohbarye <over.rye@gmail.com>
2024-04-01 00:55:35 +0900

    Try to pass example instance

--- spec/e2e/prism_spec.rb
+++ spec/e2e/prism_spec.rb
@@ -1,21 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe Pbt do
-  around do |ex|
-    Thread.report_on_exception = false
-    ex.run
-    Thread.report_on_exception = true
-  end
-
   describe "arguments" do
     it "passes a value that the given single arbitrary generates" do
-      Pbt.assert do
-        Pbt.property(self, Pbt.integer) do |n|
-          puts "called!!!!!!!!!!!!!"
-          puts n
-          raise unless n.is_a?(Integer)
+      RSpec::Matchers::BuiltIn.constants.each { |c| RSpec::Matchers::BuiltIn.const_get(c) }
+
+      r = Ractor.new do
+        loop do
+          v, msg = Ractor.receive
+          v.instance_eval msg
         end
       end
+
+      r.send([self, "expect(1).to eq 1"]) # move: true doesn't work by TypeError: can't create instance of singleton class
+
+      begin
+        r.take
+      rescue => e
+        raise e.cause
+      end
+
+      # Pbt.assert do
+      #   Pbt.property(self, Pbt.integer) do |n|
+      #     puts "called!!!!!!!!!!!!!"
+      #     puts n
+      #     raise unless n.is_a?(Integer)
+      #   end
+      # end
     end
   end
 end
